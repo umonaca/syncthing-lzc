7dd61557735bd614cbf2cb9c6c7e2b6ac4cfe53a
kevinzhow <kevinchou.c@gmail.com>
2024-11-21 00:55:42 +0800

    Clean build

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,7 +3,5 @@
 set -eu
 
 mkdir -p /lzcapp/var/syncthing/config
-mkdir -p /lzcapp/run/mnt/home/Syncthing
 
 chown -R 1000:1000 /lzcapp/var/syncthing/config
-chown -R 1000:1000 /lzcapp/run/mnt/home/Syncthing
